bede541651bb9e668c7670451584854c23a5c7b7
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-03 18:31:30 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -58,35 +58,3 @@ def setup_dispatcher(dp):
     dp.add_handler(MessageHandler(Filters.chat(TELEGRAM_SUPPORT_CHAT_ID) & Filters.reply, forward_to_user))
     return dp
 
-@My_favorit_new_client(commands=['info', 'help'])
-def category(message):
-    keyboard_category = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    key_1_1 = types.KeyboardButton('1_1')
-    keyboard_category.add(key_1_1)
-
-    bot.reply_to(message, "Привет! Я помогу подобрать товар!", reply_markup=keyboard_category)
-
-
-@My_favorit_new_client(content_types=['text'])
-def subcategory(message):
-    if message.text == "1_1":
-        keyboard_subcategory = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        key_2_1 = types.KeyboardButton('2_1')
-        keyboard_subcategory.add(key_2_1)  # key_2_1 не использовалась
-
-        bot.send_message(message.chat.id, 'Выберите подкатегорию', reply_markup=keyboard_subcategory)
-
-    elif message.text == "2_1":
-        keyboard_tovar = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        key_3_1 = types.KeyboardButton('3_1')
-        keyboard_tovar.add(key_3_1)  # key_3_1 не использовалась
-
-        bot.send_message(message.chat.id, 'Выберите товар по названию и я пришлю вам подробности',
-                         reply_markup=keyboard_tovar)
-
-    elif message.text == "3_1":  # потеряли двоеточие
-        bot.send_message(message.chat.id, "Описание товара")
-        bot.send_photo(message.chat.id, 'https://cs13.pikabu.ru/images/big_size_comm/2020-06_3/159194100716237333.jpg')  # просто укажите ссылку на картинку
-
-
-bot.polling()
