027ac288cd00b91e8f50411a6a1a110fa9bfc926
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-05 14:38:33 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,37 +1,61 @@
-var TelegramBot = require('node-telegram-bot-api');
-var bot = new TelegramBot(telegramToken, { polling: true });
-bot.on("polling_error", (m) => console.log(m));
-bot.sendMessage('Привет, хабр!', messageOptions);
-var messageOptions = {
-    parse_mode: "HTML",
-    disable_web_page_preview: false,
-    reply_markup: JSON.stringify({
-        inline_keyboard: [[{
-            text: 'Название кнопки',
-            callback_data: 'do_something'
-        }]]
-    })
-}
+import os
+from telegram.ext import CommandHandler, MessageHandler, Filters
 
-bot.onText(new RegExp('\/start'), function (message, match) {
-    // вытаскиваем id клиента из пришедшего сообщения
-    var clientId = message.hasOwnProperty('chat') ? message.chat.id : message.from.id;
-    // посылаем ответное сообщение
-    bot.sendMessage('Some message', messageOptions);
-});
+from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID
+
+def info(update, context):
+    update.message.reply_text(WELCOME_MESSAGE)
+
+    user_info = update.message.from_user.to_dict()
+
+    context.bot.send_message(
+        chat_id=TELEGRAM_SUPPORT_CHAT_ID,
+        text=f"""
+📞 Connected {user_info}.
+        """,
+    )
 
 
-bot.on('callback_query', function (message) {
-    var clientId = message.hasOwnProperty('chat') ? message.chat.id : message.from.id;
-    // То что мы записали в callback_data у кнопок приходит в message.data
-    if(message.data === 'do_something'){
-        bot.sendMessage(clientId, 'Button clicked!', messageOptions);
-    }
-});
-var buttons = [
-    botUtils.buildDefaultButton('Кнопка', 'button1'),
-    botUtils.buildShareButton('Поделиться', 'url'),
-    botUtils.buildUrlButton('Ссылка', 'link')
-];
+def forward_to_chat(update, context):
+    """{ 
+        'message_id': 5, 
+        'date': 1605106546, 
+        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
+        'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+    }"""
+    update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
 
-var messageOptions = botUtils.buildMessageOptions(buttons);
+
+def forward_to_user(update, context):
+    """{
+        'message_id': 10, 'date': 1605106662, 
+        'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
+        'reply_to_message': {
+            'message_id': 9, 'date': 1605106659, 
+            'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
+            'forward_from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
+            'forward_date': 1605106658, 
+            'text': 'g', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 
+            'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+            'from': {'id': 1440913096, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
+        }, 
+        'text': 'ggg', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 
+        'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+    }"""
+    user_id = update.message.reply_to_message.forward_from.id
+    context.bot.copy_message(
+        message_id=update.message.message_id,
+        chat_id=user_id,
+        from_chat_id=update.message.chat_id
+    )
+
+
+def setup_dispatcher(dp):
+    dp.add_handler(CommandHandler('info', info))
+    dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
+    dp.add_handler(MessageHandler(Filters.chat(TELEGRAM_SUPPORT_CHAT_ID) & Filters.reply, forward_to_user))
+    return dp
+
+
