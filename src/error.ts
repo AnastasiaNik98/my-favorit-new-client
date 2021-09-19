d80e7ad1bf6370839f326e6e2ddc6ec954aeef08
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-09-19 10:33:35 +0300

    Add files via upload

---
+++ src/error.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs';
+import * as util from 'util';
+import config from '../config/config';
+const debugFile = './config/debug.log';
+const logStdout = process.stdout;
+
+function init(bot) {
+  // overload logging to file
+  console.log = function(d) {
+    logStdout.write(util.format(d) + '\n');
+    fs.appendFile(debugFile,
+        new Date() + ': ' + util.format(d) + '\n', 'utf8',
+        function(err) {
+          if (err) throw err;
+        });
+  };
+
+  // catch uncaught exceptions to log
+  process.on('uncaughtException', (err) => {
+    console.log('=== UNHANDLED ERROR ===');
+    fs.appendFile(debugFile,
+        err.stack + '\n', 'utf8',
+        function(err) {
+          if (err) throw err;
+        });
+    console.error(new Date() + ': ' + 'Error: ', err);
+    bot.telegram.sendMessage(
+        config.staffchat_id, `An error occured, please report this to your admin: \n\n ${err}`,
+    );
+    process.exit(1);
+  });
+
+
+  // catch uncaught rejections to log
+  process.on('unhandledRejection', (err, p) => {
+    console.log('=== UNHANDLED REJECTION ===');
+    fs.appendFile(debugFile,
+        err + '\n', 'utf8',
+        function(err) {
+          if (err) throw err;
+        });
+    console.dir(new Date() + ': ' + err["stack"]);
+    bot.telegram.sendMessage(
+        config.staffchat_id, `An error occured, please report this to your admin: \n\n ${err}`,
+    );
+  });
+}
+
+export {
+  init
+};
